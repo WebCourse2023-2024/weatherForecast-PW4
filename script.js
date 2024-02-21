c78fd0b8497602e769e607050d66e1f43d78dd39
oneloveoneheart <gahetaflorent@gmail.com>
2024-02-21 20:21:37 +0100

    I wrote methods to help data displaying

--- script.js
+++ script.js
@@ -23,11 +23,38 @@ async function launchRequest(){
             console.log("Server Error")
         } else {
             const data = await response.json();
-            console.log(data);
+            displayCurrentWeatherDetails(data);
         }
     } catch (error) {
         console.error("Fetch error", error);
     }
+}
+
+function metersPerSecToKmPerHour(value) {
+    return ((value * 3600) / 1000).toFixed(1);
+}
+
+function unixToDatetime(unixTime) {
+    return new Date(unixTime * 1000);
+}
+
+function kelvinToDegree(kelvinTemp) {
+    return (kelvinTemp - 273.15).toFixed(1);
+}
+
+function displayCurrentWeatherDetails(jsonData) {
+    const currentTemp = kelvinToDegree(jsonData["current"]["temp"]);
+    const feelsLikeTemp = kelvinToDegree(jsonData["current"]["feels_like"]);
+    const sunsetTime = unixToDatetime(jsonData["current"]["sunset"]);
+    const currentWindSpeed = metersPerSecToKmPerHour(jsonData["current"]["wind_speed"]);
+    const currentWindDegree = jsonData["current"]["wind_deg"];
+    const weatherDescription = jsonData["current"]["weather"][0].description;
+    const calculationTime = unixToDatetime(jsonData["current"]["dt"]);
+
+    console.log(`Actual temperature: ${currentTemp} °C\nFeels like: ${feelsLikeTemp} °C\n` +
+        `Current wind speed: ${currentWindSpeed} km/h\nCurrent wind degree: ${currentWindDegree}°\n` +
+        `Sunset time: ${sunsetTime}\nWeather description: ${weatherDescription}\n` +
+        `Calculation time: ${calculationTime}\n`);
 }
 
 launchRequest();
